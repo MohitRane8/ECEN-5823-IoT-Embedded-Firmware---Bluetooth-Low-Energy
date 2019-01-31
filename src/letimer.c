af465475e51e4c5687d73422799ebc5be6b5e1e7
MohitRane8 <45619385+MohitRane8@users.noreply.github.com>
2019-01-30 21:33:51 -0700

    Added appropriate comments to letimer.c

--- src/letimer.c
+++ src/letimer.c
@@ -18,22 +18,24 @@ void initLETIMER(void)
 
 	/* Initialize LETIMER */
 	LETIMER_Init_TypeDef letimerInit = LETIMER_INIT_DEFAULT;
-	letimerInit.comp0Top = true;
+	letimerInit.comp0Top = true;		//To let LETIMER start counting from COMP0 value
 	LETIMER_Init(LETIMER0, &letimerInit);
 
-#if ((EnergyMode == 0) | (EnergyMode == 1) | (EnergyMode == 2))
-	/* Setting value of COMP0 - counting will start from this value */
-	uint32_t periodCount = PERIOD_MS * 8.192;
-	/* Setting value of COMP1 - where interrupt will occur and LED will turn ON */
-	uint32_t ontimeCount = ONTIME_MS * 8.192;
-#endif
-
-#if (EnergyMode == 3)
-	/* Setting value of COMP0 - counting will start from this value */
-	uint32_t periodCount = PERIOD_MS;
-	/* Setting value of COMP1 - where interrupt will occur and LED will turn ON */
-	uint32_t ontimeCount = ONTIME_MS;
-#endif
+	/* Calculating period and ontime count for EM0, EM1 and EM2 */
+	#if ((EnergyMode == 0) | (EnergyMode == 1) | (EnergyMode == 2))
+		/* Setting value of COMP0 - counting will start from this value */
+		uint32_t periodCount = PERIOD_MS * 8.192;
+		/* Setting value of COMP1 - where interrupt will occur and LED will turn ON */
+		uint32_t ontimeCount = ONTIME_MS * 8.192;
+	#endif
+	
+	/* Calculating period and ontime count for EM3 */
+	#if (EnergyMode == 3)
+		/* Setting value of COMP0 - counting will start from this value */
+		uint32_t periodCount = PERIOD_MS;
+		/* Setting value of COMP1 - where interrupt will occur and LED will turn ON */
+		uint32_t ontimeCount = ONTIME_MS;
+	#endif
 
 	/* Setting value of COMP0 - counting will start from this value */
 	LETIMER_CompareSet(LETIMER0, 0, periodCount);
@@ -61,12 +63,14 @@ void LETIMER0_IRQHandler(void)
 
 	uint32_t reason = LETIMER_IntGet(LETIMER0);
 
-	if((reason & LETIMER_IF_COMP1) == LETIMER_IF_COMP1)
+	/* Changing state of LED depending on the interrupt flag that was set */
+	if((reason & LETIMER_IF_COMP1) == LETIMER_IF_COMP1)	// set by COMP0 flag
 		gpioLed0SetOn();
 
-	else if((reason & LETIMER_IF_UF) == LETIMER_IF_UF)
+	else if((reason & LETIMER_IF_UF) == LETIMER_IF_UF)	// set by UF flag
 		gpioLed0SetOff();
 
+	/* Clearing pending interrupts */
 	LETIMER_IntClear(LETIMER0, reason);
 
 	__enable_irq();
