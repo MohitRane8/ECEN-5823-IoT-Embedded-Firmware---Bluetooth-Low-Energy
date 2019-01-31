4fb73d7929cdf66960acfd19600410023ab8a258
MohitRane8 <45619385+MohitRane8@users.noreply.github.com>
2019-01-30 21:23:12 -0700

    Added appropriate comments to clock.c

--- src/clock.c
+++ src/clock.c
@@ -1,5 +1,6 @@
 /*
  * clock.c
+ *	Clock configuration and initialization
  *
  *  Created on: Jan 29, 2019
  *      Author: Mohit
@@ -10,37 +11,28 @@
 
 void initCLOCK(void)
 {
-#if ((EnergyMode == 0) | (EnergyMode == 1) | (EnergyMode == 2))
-	/* Enable required oscillator */
-	CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
+	/* Clock configurations for EM0, EM1 and EM2 */
+	#if ((EnergyMode == 0) | (EnergyMode == 1) | (EnergyMode == 2))
+		/* Enable required oscillator */
+		CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
 
-	/* Enable necessary clocks */
-	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+		/* Enable necessary clocks */
+		CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
 
-	/* Prescalar of 4 for maximum period of 8 seconds in LETIMER0 */
-	CMU_ClockDivSet(cmuClock_LETIMER0, cmuClkDiv_4);
-#endif
+		/* Prescalar of 4 for maximum period of 8 seconds in LETIMER0 */
+		CMU_ClockDivSet(cmuClock_LETIMER0, cmuClkDiv_4);
+	#endif
 
-#if (EnergyMode == 3)
-	/* Enable required oscillator */
-	CMU_OscillatorEnable(cmuOsc_ULFRCO, true, true);
+	/* Clock configurations for EM3 */
+	#if (EnergyMode == 3)
+		/* Enable required oscillator */
+		CMU_OscillatorEnable(cmuOsc_ULFRCO, true, true);
 
-	/* Enable necessary clocks */
-	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_ULFRCO);
-#endif
-
-//	/* Enable required oscillator */
-//	CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
-////	CMU_OscillatorEnable(cmuOsc_ULFRCO, true, true);		//for ULFRCO
-//
-//	/* Enable necessary clocks */
-//	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
-////	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_ULFRCO);		//for ULFRCO
-//
-//	// Prescalar of 4
-//	CMU_ClockDivSet(cmuClock_LETIMER0, cmuClkDiv_4);
-
-
+		/* Enable necessary clocks */
+		CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_ULFRCO);
+	#endif
+	
+	/* Enabling clock of LETIMER0 */
 	CMU_ClockEnable(cmuClock_LETIMER0, true);
 }
 
