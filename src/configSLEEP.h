4e62b5ae26cfc017317b41c4ee45971c0536df18
MohitRane8 <45619385+MohitRane8@users.noreply.github.com>
2019-01-30 21:27:39 -0700

    Added appropriate comments to configSLEEP.h

--- src/configSLEEP.h
+++ src/configSLEEP.h
@@ -8,7 +8,13 @@
 #ifndef SRC_CONFIGSLEEP_H_
 #define SRC_CONFIGSLEEP_H_
 
-// define Energy Mode from 0 to 3
+/*  
+ * define Energy Mode from 0 to 3
+ *  EnergyMode 0 = EM0
+ *  EnergyMode 1 = EM1
+ *  EnergyMode 2 = EM2
+ *  EnergyMode 3 = EM3
+*/
 #define EnergyMode 2
 
 #include "sleep.h"
